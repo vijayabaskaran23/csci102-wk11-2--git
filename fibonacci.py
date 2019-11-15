dad5c0f5cd07eb32e2dd6972c93382b83aa8caec
vijayabaskaran23 <57746329+vijayabaskaran23@users.noreply.github.com>
2019-11-14 23:10:37 -0700

    created file that calculates the first 10 Fibonacci numbers

---
+++ fibonacci.py
@@ -0,0 +1,19 @@
+# fibonacci.py
+
+def fib():
+    fibs = [1, 2]
+
+    for i in range(0,9):
+
+        ''' 
+        implement Fibonacci sequence to calculate the 
+        first 10 Fibonacci numbers, note Fn = Fn-1 + Fn-2
+        '''
+
+    return fibs
+
+def main():
+    print('OUTPUT', fib())
+
+if __name__ == "__main__":
+    main()
